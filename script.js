44c4f3b01660e859d15d92d56600f71b8aa79b93
Samita <samita.khanduri.qu@gmail.com>
2025-11-27 22:01:24 +0530

    Created loading screen content

---
+++ script.js
@@ -0,0 +1,7 @@
+//here ill put my logic for loading screen alright
+//this will make it disappear after screen is loaded tbh
+var loadingScreen = document.querySelector(".loadingScreen");
+window.addEventListener('load', function() {
+  loadingScreen.style.display = 'none';
+})
+
