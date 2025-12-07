027df1887ab1e39ca0f76e7f8f4f2cca8ad7a63b
Samita <samita.khanduri.qu@gmail.com>
2025-12-07 21:01:57 +0530

    Added images, changed some styling- cat of the day and music player are now sibling elems.

---
+++ musicData.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'Song1',
+        path: 'Music/song1.mp3',
+        artist: 'Artist1',
+        cover: 'images/cover1.webp'
+    },
+    {
+        name: 'Song2',
+        path: 'Music/song2.mp3',
+        artist: 'Artist2',
+        cover: 'images/cover2.webp'
+    },
+    {
+        name: 'Song3',
+        path: 'Music/song3.mp3',
+        artist: 'Artist3',
+        cover: 'images/cover3.webp'
+    }
+]
